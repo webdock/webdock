03b0fb79962aa4f3cdea87d14ef6bbf0a18b1a88
Richard <hello@noxan.net>
2016-04-09 14:01:35 +0200

    implement containers endpoint

--- src/routes/containers.js
+++ src/routes/containers.js
@@ -1,7 +1,15 @@
 import Router from 'koa-route-class';
 
+import docker from '../docker';
+
 
 const router = new Router();
+
+
+router.get('/containers', async ctx => {
+  const containers = await docker.listContainers({all: true});
+  ctx.body = containers;
+});
 
 
 export default router;
