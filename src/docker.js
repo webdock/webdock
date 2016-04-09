c93cb30cfdbe690c105dce06f551cd8967d0d28f
Richard <hello@noxan.net>
2016-04-09 14:06:48 +0200

    add reference to dockerode readme for connection setup

--- src/docker.js
+++ src/docker.js
@@ -1,6 +1,9 @@
 import Docker from 'dockerode-promise';
 
 
+// see dockerode readme on how to configure docker connection
+// https://github.com/apocas/dockerode#getting-started
 const docker = new Docker();
+
 
 export default docker;
