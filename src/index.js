a2259893353fffeaf00dba0bb1173a9b5dd59fa2
Richard <hello@noxan.net>
2016-04-09 11:48:36 +0200

    test docker inspect results

--- src/index.js
+++ src/index.js
@@ -2,4 +2,18 @@ import Docker from 'dockerode';
 
 const docker = new Docker();
 
-docker.listContainers({all: true}, (err, containers) => console.log(err, containers));
+
+docker.listContainers({all: true}, (err, containers) => {
+  if (containers.length > 0) {
+    console.log(containers);
+
+    if (containers.length > 0) {
+      const containerId = containers[0].Id;
+      const container = docker.getContainer(containerId);
+
+      container.inspect((err, data) => {
+        console.log(err, data);
+      });
+    }
+  }
+});
