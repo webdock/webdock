5c113a4c2b01f80e808d150f27c893ef597d912c
Richard <hello@noxan.net>
2016-04-09 13:45:20 +0200

    extract router wrapper to own module

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,29 +1,5 @@
-import route from 'koa-route';
+import Router from 'koa-route-class';
 
-
-class Router {
-  constructor() {
-    this.routeMap = new Map();
-  }
-
-  get(path, fn) {
-    this.routeMap.set(path, fn);
-  }
-
-  routes() {
-    const self = this;
-    const dispatch = async (ctx, next) => {
-      const { app } = ctx;
-
-      for (const [path, fn] of self.routeMap) {
-        app.use(route.get(path, fn));
-      }
-
-      await next();
-    }
-    return dispatch;
-  }
-}
 
 
 const router = new Router();
