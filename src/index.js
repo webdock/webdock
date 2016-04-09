a0d1dbf30f76b246157bba5109b86a49d65c4b0a
Richard <hello@noxan.net>
2016-04-09 10:39:43 +0200

    port current code to es6

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-const Docker = require('dockerode');
+import Docker from 'dockerode';
 
 const docker = new Docker();
 
-docker.listContainers({all: true}, function (err, containers) {
-  console.log(err, containers);
-});
+docker.listContainers({all: true}, (err, containers) => console.log(err, containers));
