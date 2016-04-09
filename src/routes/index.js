50d9b30d61e0342605308b142f230c5552fa7dc2
Richard <hello@noxan.net>
2016-04-09 12:37:59 +0200

    add koa-route and wrap it into a map

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,19 @@
-export default function (ctx) {
+import route from 'koa-route';
+
+
+const routes = new Map();
+
+routes.set('/', ctx => {
   ctx.body = 'hello webdock';
+});
+
+
+export default async function (ctx, next) {
+  const { app } = ctx;
+
+  for (const [path, fn] of routes) {
+    app.use(route.get(path, fn));
+  }
+
+  await next();
 };
