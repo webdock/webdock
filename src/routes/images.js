45e3ea2ebfd4b858bfd6b024839c22f87af7d2a5
Richard <hello@noxan.net>
2016-04-10 13:51:18 +0200

    adjust format of image list route for ember

--- src/routes/images.js
+++ src/routes/images.js
@@ -7,7 +7,21 @@ const router = new Router();
 
 
 router.get('/images', async ctx => {
-  ctx.body = await docker.listImages();
+  const dockerImages = await docker.listImages();
+  ctx.body = {
+    images: dockerImages.map(image => {
+      return {
+        id: image.Id,
+        parentId: image.ParentId,
+        repoTags: image.RepoTags,
+        repoDigests: image.RepoDigests,
+        created: image.Created,
+        size: image.Size,
+        virtualSize: image.VirtualSize,
+        labels: image.Labels,
+      };
+    }),
+  }
 });
 
 
