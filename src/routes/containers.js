e1777650d206768519fc3d6c3d82b174f49edcae
Richard <hello@noxan.net>
2016-04-11 23:08:46 +0200

    define schema for json api for container and use it

--- src/routes/containers.js
+++ src/routes/containers.js
@@ -1,7 +1,7 @@
 import Router from 'koa-route-class';
 
+import containerSchema from '../schemas/container';
 import docker from '../docker';
-import { formatImageId } from '../utils/images';
 
 
 const router = new Router({
@@ -13,25 +13,7 @@ router.get('/', async ctx => {
   const paramAll = true; // ctx.query.all !== undefined;
   const containers = await docker.listContainers({all: paramAll});
 
-  ctx.body = {
-    data: containers.map(container => {
-      return {
-        type: 'containers',
-        id: container.Id,
-        attributes: {
-          names: container.Names,
-          image: container.Image.match(/sha256\:/) ? '<none>' : container.Image,
-          imageId: formatImageId(container.ImageID),
-          status: container.Status,
-          created: container.Created,
-          command: container.Command,
-          ports: container.Port,
-        },
-        relationships: {
-        }
-      };
-    }),
-  };
+  ctx.body = containerSchema.serialize(containers);
 });
 
 router.get('/:id', async (ctx, containerId) => {
---
+++ src/schemas/container.js
@@ -0,0 +1,20 @@
+import Schema from 'jsonapi';
+
+import { formatImageId } from '../utils/images';
+
+
+const containerSchema = new Schema({
+  id: 'Id',
+  type: 'containers',
+  attributes: {
+    names: 'Names',
+    image: (obj) => obj.Image.match(/sha256\:/) ? '<none>' : obj.Image,
+    imageId: (obj) => formatImageId(obj.ImageID),
+    status: 'Status',
+    created: 'Created',
+    command: 'Command',
+    ports: 'Port',
+  },
+});
+
+export default containerSchema;
