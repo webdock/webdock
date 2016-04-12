9d99eac93cf122d2cb228f16c1d05172b7d0fa4f
Richard <hello@noxan.net>
2016-04-12 14:59:21 +0200

    prototype implementation of update events for containers via socket

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import multiplex from 'primus-multiplex';
 import Koa from 'koa';
 import cors from 'koa-cors';
 
+import docker from './docker';
 import routes from './routes';
+import containerSchema from './schemas/container';
 
 
 const app = new Koa();
@@ -21,3 +23,23 @@ server.listen(3000);
 const primus = new Primus(server);
 primus.use('multiplex', multiplex);
 primus.library();
+
+
+const events = primus.channel('events');
+
+
+(async () => {
+  const dockerEvents = await docker.getEvents();
+
+  dockerEvents.on('data', (async data => {
+    const dockerEvent = JSON.parse(data.toString('utf8'));
+
+    if (dockerEvent.Type === 'container') {
+      const dockerContainerRef = await docker.getContainer(dockerEvent.Actor.ID);
+
+      const dockerContainer = await dockerContainerRef.inspect();
+
+      events.write(containerSchema.serialize(dockerContainer));
+    }
+  }));
+})();
