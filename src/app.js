85dbd9797d8b7ec6c93e1da47ad50d46d58e201f
Richard <hello@noxan.net>
2016-05-03 11:04:24 +0200

    ignore eslint for server ready log

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = new Koa();
 
 export const server = http.createServer(app.callback());
 server.listen(3000);
-console.log('Server listening to http://127.0.0.1:3000/.');
+console.log('Server listening to http://127.0.0.1:3000/.'); // eslint-disable-line no-console
 
 
 export default app;
