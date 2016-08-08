d950e5ecb0f3f04e442a10fc7945301836a4bd29
Richard <hello@noxan.net>
2016-08-08 19:03:47 +0200

    merge app and index files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
+import http from 'http';
+import Koa from 'koa';
 import cors from 'kcors';
 import jwt from 'koa-jwt';
 import pathMatch from 'koa-path-match';
 import bodyParser from 'koa-bodyparser';
 
-import app from './app';
 import routes from './routes';
 import { authenticateRoute } from './routes/authenticate';
 import { SECRET_KEY } from './constants';
 
 const route = pathMatch();
+
+const app = new Koa();
 
 app.use(cors({
   allowHeaders: ['Authorization', 'Content-Type'],
@@ -20,3 +23,7 @@ app.use(route('/api/authenticate').post(authenticateRoute));
 app.use(jwt({ secret: SECRET_KEY }).unless({ method: 'OPTIONS' }));
 
 app.use(routes);
+
+const server = http.createServer(app.callback());
+server.listen(3000);
+console.log('Server listening to http://127.0.0.1:3000/.'); // eslint-disable-line no-console
