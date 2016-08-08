7a2bb583e3d7a5e9ea4cd160ca688367fbb6f875
Richard <hello@noxan.net>
2016-08-08 12:24:58 +0200

    cleanup authenticate call code and add validation errors for missing email and password

--- src/routes/authenticate.js
+++ src/routes/authenticate.js
@@ -5,19 +5,29 @@ import { SECRET_KEY } from '../constants';
 
 const userArray = Object.values(users);
 
+const error = (ctx, message, statusCode = 400) => {
+  ctx.status = statusCode;
+  ctx.body = { message, statusCode };
+};
+
 export const authenticateRoute = ctx => {
   const { email, password } = ctx.request.body;
 
+  if (!email) {
+    return error(ctx, 'Email is missing.');
+  }
+  if (!password) {
+    return error(ctx, 'Password is missing.');
+  }
+
   const user = userArray.find(el => el.email === email && el.password === password);
 
-  if (user) {
-    const token = jwt.sign(user, SECRET_KEY, {
-      expiresIn: 60 * 60 * 24 * 30, // 60s * 60m * 24h * 30d
-    });
-
-    ctx.body = { token };
-  } else {
-    ctx.status = 400;
-    ctx.body = 'Invalid email or password.';
+  if (!user) {
+    return error(ctx, 'Invalid email or password.');
   }
+
+  const token = jwt.sign(user, SECRET_KEY, {
+    expiresIn: 60 * 60 * 24 * 30, // 60s * 60m * 24h * 30d
+  });
+  ctx.body = { token };
 };
